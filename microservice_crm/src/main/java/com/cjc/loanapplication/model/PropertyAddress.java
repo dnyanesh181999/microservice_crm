7065212c28ca406ef9a420d5b1f6d3fc24f86ceb
dnyanesh181999 <dnyaneshdeshmukh506@gmail.com>
2024-06-06 14:46:11 +0530

    Second commit By Pranav Deshmukh

---
+++ microservice_crm/src/main/java/com/cjc/loanapplication/model/PropertyAddress.java
@@ -0,0 +1,11 @@
+package com.cjc.loanapplication.model;
+
+public class PropertyAddress {
+
+	private Integer propertyAddressId;
+	private String propertyAreaName;
+	private String propertyCityName;
+	private String propertyDistrict;
+	private String propertyState;
+	private Long propertyPinCode;
+}
