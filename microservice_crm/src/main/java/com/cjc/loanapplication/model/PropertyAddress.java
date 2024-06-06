1343a4c54c2d394806a6d3acc1199c6723f352e1
Ravikumar Shrikhande <ravishrikhande3@gmail.com>
2024-06-06 20:41:49 +0530

    By Ravi

--- microservice_crm/src/main/java/com/cjc/loanapplication/model/PropertyAddress.java
+++ microservice_crm/src/main/java/com/cjc/loanapplication/model/PropertyAddress.java
@@ -1,7 +1,21 @@
 package com.cjc.loanapplication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PropertyAddress {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer propertyAddressId;
 	private String propertyAreaName;
 	private String propertyCityName;
--- microservice_crm/src/main/java/com/cjc/loanapplication/model/PropertyDetails.java
+++ microservice_crm/src/main/java/com/cjc/loanapplication/model/PropertyDetails.java
@@ -1,9 +1,11 @@
 package com.cjc.loanapplication.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +23,6 @@ public class PropertyDetails {
 	private String constructionArea;
 	private Double propertyPrice;
 	private Double constructionPrice;
+	@OneToOne(cascade = CascadeType.ALL)
 	private PropertyAddress propertyAddress;
 }
